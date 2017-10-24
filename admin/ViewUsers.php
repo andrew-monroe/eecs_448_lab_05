09ff8e9c0373c64044a0d1c06a8e7def1f4bb807
Andy Monroe <amonroe805@gmail.com>
2017-10-23 21:16:40 -0500

    Create a users#view page

---
+++ admin/ViewUsers.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+  <title>View Users</title>
+</head>
+<body>
+<h1>List of Users</h1>
+<table>
+<thead>
+  <tr>
+    <th>user_id</th>
+  </tr>
+</thead>
+
+<tbody>
+<?php
+$connection = new mysqli("mysql.eecs.ku.edu", 'amonroe', 'P@$$word123','amonroe');
+
+if($connection->connect_error) {
+  die("Connection failed: " . $connection->connect_error);
+}
+
+$query = "SELECT user_id FROM Users";
+
+if ($users = $connection->query($query)) {
+  while($row = $users->fetch_assoc()) {
+    echo "<tr>";
+    echo "  <td>" . $row['user_id'] . "</td>";
+    echo "</tr>";
+  }
+
+  $users->free();
+} else {
+  echo "Error: " . $query . "<br>" . $connection->error;
+}
+
+$connection->close();
+?>
+</tbody>
+</table>
+
+</body>
+</html>
